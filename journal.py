24f277800891c1d7f142c83e366140ad9e378b4b
Matthew Cranford <m.cranford13@gmail.com>
2020-12-17 16:58:11 -0700

    Error handling
    Minor error handling

--- journal.py
+++ journal.py
@@ -17,16 +17,29 @@ GUI Setup
 '''
 sg.change_look_and_feel("Dark")
 
-layout = [
+gui_layout = [
          [sg.Text("", size=(20,1), key='date', justification='left'), 
          sg.In(key='cal', enable_events=True, visible=False), 
           sg.CalendarButton('Calendar', target="cal",
           font=('MS Sans Serif', 10, 'bold'),key='calendar', format=('%B, %d, %Y'))],
-          [sg.Multiline(size=(30,12), key='guiMulLine')],
+          [sg.Multiline(key='guiMulLine')],
         [sg.Submit(auto_size_button=True)]     
         ]
 
-gui = sg.Window('Simple Journal', layout, size=(300, 300))
+gui = sg.Window('Simple Journal', gui_layout, size=(300, 300))
+
+cal_error_layout = [
+
+        [sg.Text("", size=(20,2), key='error', justification='center')],
+        [sg.Ok()]
+
+]
+entry_error_layout = [
+
+        [sg.Text("", size=(20,2), key='error', justification='center')],
+        [sg.Ok()]
+
+]
 
 # Functions
 def SaveEntry(date,entry):
@@ -37,20 +50,31 @@ def SaveEntry(date,entry):
     
     f.write(f"{date}\n")
     f.write("==================\n")
-
-    
-
-    
     f.close()
     
-   
+def ErrorHandler(message, param):
+    if param == "cal":
+        error = sg.Window("", cal_error_layout, size=(225,75), finalize=True)
+    else:
+        error = sg.Window("", entry_error_layout, size=(225,75), finalize=True)
+
+    error["error"].update(f"Error!\n{message}")
+
+    while True:
+        event, values = error.read()
+        if event in (None, 'Exit') or event in ("Ok"):
+            break
+    error.close()
+
+
+
 def main():
     if not os.path.exists("entries"):
         os.mkdir("entries")
 
     while True:
         event, values = gui.read()
-        print(event,values)
+        #print(event,values)
         if event in (None, 'Exit'):
             break
         if event in ("cal"):
@@ -58,16 +82,16 @@ def main():
         if event in ("Submit", "guiMulLine"):
 
             if values['cal'] == '':
-                print("Please select a date to continue.")
+                ErrorHandler("Date cannot be blank.", "cal")
             elif values['guiMulLine'] == '\n':
-                print("Entry is blank!")
+                ErrorHandler("Entry is blank!", "entry")
             else:
                 selectedDate = values['cal']
                 entryText = values["guiMulLine"]
             
                 SaveEntry(selectedDate, entryText)
             
-
+            
             
 
     gui.close() 
